d859771b96313c076355cc8a0499a2313a0a38c3
Jackson Luis de Oliveira Conceição <jlconceicao@uneb.br>
2025-06-16 16:03:11 -0300

    teste correção vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Relatorio from '../views/RelatorioView.vue'
 import Calculadora from '../views/CalculadoraView.vue'
 
@@ -17,18 +17,10 @@ const routes: Array<RouteRecordRaw> = [
     name: 'calculadora',
     component: Calculadora
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(), // usa hash mode para evitar problemas de deploy
   routes
 })
 
